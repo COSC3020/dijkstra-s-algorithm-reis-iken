1c8579a8b515d1f8b1a1c08107c64b59069c23f2
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 16:07:32 -0600

    code.js

--- code.js
+++ code.js
@@ -1,28 +1,19 @@
 function dijkstra(graph, sourceNode) {
-    let distanceResult = {};
-    let unvisited = new Set(Object.keys(graph));
-    for (let node in graph) {
-        distanceResult[node] = node === sourceNode ? 0 : Infinity;
+    let dist = {};
+    let pq = new PriorityQueue();
+    for (let vertex in graph) {
+        dist[vertex] = vertex === sourceNode ? 0 : Infinity;
+        pq.enqueue(vertex, dist[vertex]);
     }
-    while (unvisited.size > 0) {
-        let minNode = null;
-        for (let node of unvisited) {
-            if (minNode === null || distanceResult[node] < distanceResult[minNode]) {
-                minNode = node;
-            }
-        }
-        if (minNode === null) {
-            break;
-        }
-        unvisited.delete(minNode);
-        for (let neighbor of Object.keys(graph[minNode])) {
-            if (unvisited.has(neighbor)) {
-            let distance = distanceResult[minNode] + graph[minNode][neighbor];
-            if (distance < distanceResult[neighbor]) {
-            distanceResult[neighbor] = distance;
-                }
+    while (!pq.isEmpty()) {
+        let u = pq.dequeue();
+        for (let neighbor in graph[u]) {
+            let alt = dist[u] + graph[u][neighbor];
+            if (alt < dist[neighbor]) {
+                dist[neighbor] = alt;
+                pq.enqueue(neighbor, alt);
             }
         }
     }
-    return distanceResult;
+    return dist;
 }
