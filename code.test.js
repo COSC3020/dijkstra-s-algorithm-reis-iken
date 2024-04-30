e131582f0b632c4ca4f6c3179b89540c99b531de
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 16:09:31 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,16 +1,14 @@
-let fs = require('fs');
-let jsc = require('jsverify');
-
-// Dijkstra's algorithm implementation
+const fs = require('fs');
+const jsc = require('jsverify');
 function dijkstra(graph, sourceNode) {
-    let distanceResult = {};
-    let unvisited = new Set(Object.keys(graph));
-    for (let node in graph) {
+    const distanceResult = {};
+    const unvisited = new Set(Object.keys(graph));
+    for (const node in graph) {
         distanceResult[node] = node === sourceNode ? 0 : Infinity;
     }
     while (unvisited.size > 0) {
         let minNode = null;
-        for (let node of unvisited) {
+        for (const node of unvisited) {
             if (minNode === null || distanceResult[node] < distanceResult[minNode]) {
                 minNode = node;
             }
@@ -19,27 +17,27 @@ function dijkstra(graph, sourceNode) {
             break;
         }
         unvisited.delete(minNode);
-        for (let neighbor of Object.keys(graph[minNode])) {
+        for (const neighbor of Object.keys(graph[minNode])) {
             if (unvisited.has(neighbor)) {
-                let distance = distanceResult[minNode] + graph[minNode][neighbor];
+                const distance = distanceResult[minNode] + graph[minNode][neighbor];
+                
                 if (distance < distanceResult[neighbor]) {
                     distanceResult[neighbor] = distance;
                 }
             }
         }
     }
+    
     return distanceResult;
 }
-
-// Property-based test for Dijkstra's algorithm
-let testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer(1, 1000))), jsc.string, function(graph, sourceNode) {
-    let result = dijkstra(graph, sourceNode);
+const testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer(1, 1000))), jsc.string, function(graph, sourceNode) {
+    const result = dijkstra(graph, sourceNode);
     function checkDistances(distances) {
-        for (let node in distances) {
+        for (const node in distances) {
             if (distances[node] === Infinity && node !== sourceNode) {
                 return false;
             }
-            for (let neighbor in graph[node]) {
+            for (const neighbor in graph[node]) {
                 if (distances[neighbor] > distances[node] + graph[node][neighbor]) {
                     return false;
                 }
@@ -49,6 +47,4 @@ let testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer(1, 1000))), jsc.stri
     }
     return checkDistances(result);
 });
-
-// Run the property-based test
 jsc.assert(testDijkstra);
