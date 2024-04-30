dbfb5bf34098c5fec418455f4f4ba71616d9d758
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 14:07:17 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    let distanceResult = {};
+    let unvisited = new Set(Object.keys(graph));
+    for (let node in graph) {
+        distanceResult[node] = Infinity;
+    }
+    distanceResult[sourceNode] = 0;
+    while (unvisited.size > 0) {
+        let minNode = null;
+        for (let node of unvisited) {
+            if (minNode === null || distanceResult[node] < distanceResult[minNode]) {
+                minNode = node;
+            }
+        }
+        if (minNode === null) {
+            break; }
+        visited[minNode] = true;
+        unvisited.delete(minNode);
+        for (let neighbor in graph[minNode]) {
+            let distance = distanceResult[minNode] + graph[minNode][neighbor];
+            if (distance < distanceResult[neighbor]) {
+                distanceResult[neighbor] = distance;
+            }
+        }
+    }
+    return distanceResult;
 }
