541d04280404a0968633f01f029fdd65d0fa2186
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 16:13:29 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,41 +1,14 @@
-const fs = require('fs');
-const jsc = require('jsverify');
-function dijkstra(graph, sourceNode) {
-    const distanceResult = {};
-    const unvisited = new Set(Object.keys(graph));
-    for (const node in graph) {
-        distanceResult[node] = node === sourceNode ? 0 : Infinity;
-    }
-    while (unvisited.size > 0) {
-        let minNode = null;
-        for (const node of unvisited) {
-            if (minNode === null || distanceResult[node] < distanceResult[minNode]) {
-                minNode = node;
-            }
-        }
-        if (minNode === null) {
-            break;
-        }
-        unvisited.delete(minNode);
-        for (const neighbor of Object.keys(graph[minNode])) {
-            if (unvisited.has(neighbor)) {
-                const distance = distanceResult[minNode] + graph[minNode][neighbor];
-                
-                if (distance < distanceResult[neighbor]) {
-                    distanceResult[neighbor] = distance;
-                }
-            }
-        }
-    }
-    
-    return distanceResult;
-}
 const testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer(1, 1000))), jsc.string, function(graph, sourceNode) {
     const result = dijkstra(graph, sourceNode);
     function checkDistances(distances) {
+        const visitedNodes = new Set();
         for (const node in distances) {
-            if (distances[node] === Infinity && node !== sourceNode) {
-                return false;
+            if (distances[node] === Infinity) {
+                if (node !== sourceNode) {
+                    return false;
+                }
+            } else {
+                visitedNodes.add(node);
             }
             for (const neighbor in graph[node]) {
                 if (distances[neighbor] > distances[node] + graph[node][neighbor]) {
@@ -43,8 +16,7 @@ const testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer(1, 1000))), jsc.st
                 }
             }
         }
-        return true;
+        return visitedNodes.size === Object.keys(graph).length;
     }
     return checkDistances(result);
 });
-jsc.assert(testDijkstra);
