29cdf4fabe14a82a6dab8c64278f1c5e67fa9be4
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 14:12:38 -0600

    code.test.js

---
+++ code.test.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+let jsc = require('jsverify');
+eval(fs.readFileSync('code.js')+'');
+
+let testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer)), jsc.string, function(graph, sourceNode) {
+    let result = dijkstra(graph, sourceNode);
+
+    function checkDistances(distances) {
+        for (let node in distances) {
+            if (distances[node] === Infinity && node !== sourceNode) {
+                return false;
+            }
+            for (let neighbor in graph[node]) {
+                if (distances[neighbor] > distances[node] + graph[node][neighbor]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    return checkDistances(result);
+});
+
+jsc.assert(testDijkstra);
