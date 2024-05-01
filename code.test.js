661bac0b358af2a98383def50d465b5cd4ed721b
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 22:41:19 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -1,27 +1,17 @@
-let fs = require('fs');
-let jsc = require('jsverify');
+const fs = require('fs');
+const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
-const testDijkstra = jsc.forall(jsc.dict(jsc.dict(jsc.integer(1, 1000))), jsc.string, function(graph, sourceNode) {
-    const result = dijkstra(graph, sourceNode);
-    function checkDistances(distances) {
-        const visitedNodes = new Set();
-        for (const node in distances) {
-            if (distances[node] === Infinity) {
-                if (node !== sourceNode) {
-                    return false;
-                }
-            } else {
-                visitedNodes.add(node);
-            }
-            for (const neighbor in graph[node]) {
-                if (distances[node] !== Infinity && distances[neighbor] > distances[node] + graph[node][neighbor]) {
-                    return false;
-                }
-            }
-        }
-        return visitedNodes.size === Object.keys(graph).length;
-    }
-    return checkDistances(result);
-});
 
-jsc.assert(testDijkstra);
+let testGraph1 = [[0, 1, 3], [2, 0, 1], [10, 10, 0]];
+let testGraph2 = [[0, 1, 3], [2, 0, 4], [10, 10, 0]];
+let testGraph3 = [[-1, 2, 0], [0, -4, 0], [0, 0, 0]];
+let testGraph4 = [[100, 4, 20], [1, 0, 30], [0, 0, 0]];
+
+assert(
+  JSON.stringify(dijkstra(testGraph1, 0)) === JSON.stringify([0, 1, 2]));
+assert(
+  JSON.stringify(dijkstra(testGraph2, 0)) === JSON.stringify([0, 1, 3]));
+assert(
+  dijkstra(testGraph3, 0) == -1);
+assert(
+  JSON.stringify(dijkstra(testGraph4, 1)) === JSON.stringify([1, 0, 21]));
