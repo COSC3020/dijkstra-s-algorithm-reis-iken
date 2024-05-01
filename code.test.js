ed99c159cf8dd50f8388365d0a17a33bf0e51073
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 23:00:28 -0600

    code.test.js

--- code.test.js
+++ code.test.js
@@ -17,14 +17,11 @@ function convertToGraph(adjacencyMatrix) {
 
 let testGraph1 = convertToGraph([[0, 1, 3], [2, 0, 1], [10, 10, 0]]);
 let testGraph2 = convertToGraph([[0, 1, 3], [2, 0, 4], [10, 10, 0]]);
-let testGraph3 = convertToGraph([[-1, 2, 0], [0, -4, 0], [0, 0, 0]]);
-let testGraph4 = convertToGraph([[100, 4, 20], [1, 0, 30], [0, 0, 0]]);
+let testGraph3 = convertToGraph([[100, 4, 20], [1, 0, 30], [0, 0, 0]]);
 
 assert(
   JSON.stringify(dijkstra(testGraph1, 0)) === JSON.stringify({ '0': 0, '1': 1, '2': 2 }));
 assert(
   JSON.stringify(dijkstra(testGraph2, 0)) === JSON.stringify({ '0': 0, '1': 1, '2': 3 }));
 assert(
-  dijkstra(testGraph3, 0) == -1);
-assert(
-  JSON.stringify(dijkstra(testGraph4, 1)) === JSON.stringify({ '0': 1, '1': 0, '2': 21 }));
+  JSON.stringify(dijkstra(testGraph3, 1)) === JSON.stringify({ '0': 1, '1': 0, '2': 21 }));
